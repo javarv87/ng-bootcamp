76e4af4fa68281d0d8d2353bdf3fb3a6bcc9a3e0
Javier Ruiz Vázquez <java.rv87@gmail.com>
2020-06-26 10:18:51 -0500

    Add routing and create components login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { MainService } from './services/main-service.service';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodoContainerComponent } from './components/todo-container/todo-container.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'todolist',
+    component: TodoContainerComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoFormComponent,
-    TodoContainerComponent
+    TodoContainerComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [MainService],
   bootstrap: [AppComponent]
